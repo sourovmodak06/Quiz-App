ea491cf7625853094f386b2ee62c16993f80bb8b
sourovmodak06 <sourovmodak650@gmail.com>
2023-03-18 20:23:36 +0600

    Quiz App style ok

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('hi');
+const startQuiz = document.getElementById('start-quiz');
+const quizRules = document.getElementById('quiz-rules');
+const exitQuiz = document.getElementById('exit-quiz');
+const continueQuiz = document.getElementById('continue')
+startQuiz.addEventListener('click', () =>{
+    quizRules.classList.remove('hidden');
+    startQuiz.classList.add('hidden');
+})
+exitQuiz.addEventListener('click', ()=>{
+    startQuiz.classList.remove('hidden');
+    quizRules.classList.add('hidden');
+})
+
+
